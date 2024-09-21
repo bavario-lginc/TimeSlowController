ead79c780b8bfe59848e82228d36b372e26f1bcf
aba4gamer <ambernalalvarez@outlook.com>
2024-09-21 20:24:34 +0200

    No more OsReports

--- source/TimeSlowController.cpp
+++ source/TimeSlowController.cpp
@@ -7,31 +7,21 @@ TimeSlowController::TimeSlowController(const char *pName) : LiveActor(pName)
 void TimeSlowController::initAfterPlacement()
 {
     MR::createTimeStopScreenEffect();
-    OSReport("Effect created\n");
 }
 void TimeSlowController::init(const JMapInfoIter &rIter)
 {
-    OSReport("Hello there\n");
     MR::useStageSwitchReadA(this, rIter);
-    OSReport("Works 1\n");
     MR::useStageSwitchReadB(this, rIter);
-    OSReport("Works 2\n");
     MR::initDefaultPos(this, rIter);
-    OSReport("Works 3\n");
     MR::connectToSceneMapObjMovement(this);
-    OSReport("Works 4\n");
     MR::invalidateClipping(this);
-    OSReport("Works 5\n");
     MR::getJMapInfoArg0NoInit(rIter, &mTimer);
     MR::getJMapInfoArg1NoInit(rIter, &willKill);
-    OSReport("Works 6\n");
     cTimer = mTimer;
     makeActorAppeared();
-    OSReport("Works 7\n");
 }
 void TimeSlowController::control()
 {
-    OSReport("I'm being controlled\n");
     if (mTimer != -1)
     {
         if (MR::isOnSwitchA(this))
@@ -40,14 +30,12 @@ void TimeSlowController::control()
             {
                 MR::onTimeStopScreenEffect();
                 MR::onSwitchB(this);
-                OSReport("A is ON\n");
                 executeOn = true;
             }
             else if (!cTimer)
             {
                 MR::offTimeStopScreenEffect();
                 MR::offSwitchB(this);
-                OSReport("A is OFF\n");
                 executeOn = true;
                 if (willKill > 0)
                     kill();
@@ -64,13 +52,11 @@ void TimeSlowController::control()
             MR::onTimeStopScreenEffect();
             MR::onSwitchA(this);
             executeOn = true;
-            OSReport("B is ON\n");
         }
         if (!MR::isOnSwitchA(this) && executeOn)
         {
             MR::offTimeStopScreenEffect();
             MR::offSwitchB(this);
-            OSReport("B is OFF\n");
             executeOn = false;
             if (willKill > 0)
                 kill();
