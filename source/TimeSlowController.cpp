a5125bee9c9b69a0e159165c38997273e7979d1a
aba4gamer <ambernalalvarez@outlook.com>
2024-10-30 21:54:30 +0100

    Bugfixes

--- source/TimeSlowController.cpp
+++ source/TimeSlowController.cpp
@@ -40,6 +40,7 @@ void TimeSlowController::control()
             MR::offTimeStopScreenEffect();
             MR::offSwitchB(this);
             
+            wasOnSwitchFlag = false;
             executeOn = false;
             if (willKill)
                 kill();
